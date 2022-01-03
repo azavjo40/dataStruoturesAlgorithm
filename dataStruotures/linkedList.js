c34e9387149449e3fb9dd0fddcb7a7df9997aa26
azavjo40 <pabek92@gmail.com>
2022-01-03 06:54:57 +0100

    add

--- dataStruotures/linkedList.js
+++ dataStruotures/linkedList.js
@@ -1,3 +1,85 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  append(data) {
+    const node = new Node(data)
+    if (this.tail) this.tail.next = node
+    this.tail = node
+    if (!this.head) this.head = node
+  }
+
+  prepend(data) {
+    const node = new Node(data, this.head)
+    this.head = node
+    if (!this.tail) this.tail = node
+  }
+
+  toArray() {
+    const output = []
+    let current = this.head
+    while (current) {
+      output.push(current)
+      current = current.next
+    }
+    return output
+  }
+
+  insertAfter(after, data) {
+    const found = this.find(after)
+    if (!found) return "not found"
+    this.remove(found.next.data)
+    found.next = new Node(data, found.next)
+  }
+
+  find(data) {
+    if (!this.head) return "not found"
+    let current = this.head
+    while (current) {
+      if (current.data === data) return current
+      current = current.next
+    }
+    return "not found"
+  }
+
+  remove(data) {
+    if (!this.head) return "not found"
+    while (this.head && this.head.data === data) {
+      this.head = this.head.next
+    }
+    let current = this.head
+    while (current.next) {
+      if (current.next.data === data) current.next = current.next.next
+      else current = current.next
+      if (this.tail.data === data) this.tail = current
+    }
+  }
+}
+
+const list = new LinkedList()
+list.append("my")
+list.append("name")
+list.append("is not")
+list.prepend("hi")
+list.append("Azam")
+list.append("how are you ?")
+list.append("goode")
+list.append("goode and you ?")
+list.insertAfter("name", "is")
+list.insertAfter("is", "Adam")
+console.log(list.toArray())
+console.log(list.find("name"))
+console.log(JSON.stringify(list))
+
 class LinkedList {
   constructor() {
     this.size = 0
--- main.js
+++ main.js
@@ -1,56 +1,62 @@
-class BinaryTree {
-  constructor() {
-    this.root = null
-  }
-  add(value) {
-    if (!this.root) {
-      this.root = new TreeNode(value)
-    } else {
-      let node = this.root
-      let newNode = new TreeNode(value)
-
-      while (node) {
-        if (value > node.value) {
-          if (!node.right) break
-          node = node.right
-        } else {
-          if (!node.left) break
-          node = node.left
-        }
-      }
-
-      if (value > node.value) {
-        node.right = newNode
-      } else {
-        node.left = newNode
-      }
-    }
-  }
-
-  getAll() {
-    return this.root
-  }
-
-  print(root = this.root) {
-    if (!root) return true
-    console.log(root.value)
-    this.print(root.left)
-    this.print(root.right)
-  }
-}
-
-class TreeNode {
-  constructor(value) {
-    this.value = value
+class Node {
+  constructor(data) {
+    this.data = data
     this.left = null
     this.right = null
   }
 }
 
+class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+  appand(data) {
+    const newNode = new Node(data)
+    if (!this.root) this.root = newNode
+    this.appandNode(this.root, newNode)
+  }
+  appandNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (!node.left) node.left = newNode
+      else this.appandNode(node.left, newNode)
+    } else {
+      if (!node.right) node.right = newNode
+      else this.appandNode(node.right, newNode)
+    }
+  }
+
+  remove(data) {
+    let current = this.root
+    while (current) {
+      if (0 >= current.data) {
+        if (!current.right) break
+        current = current.right
+        if (data === current.data) {
+          delete current.data
+        }
+      } else {
+        if (!current.left) break
+        current = current.left
+        if (data === current.data) {
+          delete current.data
+        }
+      }
+    }
+  }
+  print() {
+    console.log(this.root)
+  }
+}
+
 const tree = new BinaryTree()
-tree.add(2)
-tree.add(5)
-tree.add(6)
-tree.add(8)
+tree.appand(10)
+tree.appand(9)
+tree.appand(8)
+tree.appand(7)
+tree.appand(6)
+tree.appand(5)
+tree.appand(3)
+tree.appand(2)
+tree.remove(10)
+tree.appand(2)
 tree.print()
-console.log(tree.getAll())
