55349a487db5667edf67c9b2caaf82d52ac6f884
azavjo40 <pabek92@gmail.com>
2022-02-09 12:22:28 +0100

    add observable

---
+++ MyObservable/observable.js
@@ -0,0 +1,35 @@
+class Observable {
+  constructor(subscriptionFun) {
+    this.subscriptionFun = subscriptionFun
+    this.nextData = []
+    this.errorData = []
+  }
+  subscribe(next, error, complete) {
+    try {
+      this.subscriptionFun(next, error)
+      if (complete) {
+        complete()
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
+const observable = new Observable((next, error) => {
+  next("hello")
+  next("how are you ?")
+})
+
+observable.subscribe(
+  item => console.log("1", item),
+  null,
+  () => console.log("complete")
+)
+observable.subscribe(item => console.log("2", item))
+
+observable.subscribe(
+  item => console.log("3", item),
+  null,
+  () => console.log("complete")
+)
